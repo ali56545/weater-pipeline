5f76b5b5cd4b7531d4c4079a56b46d889c3ae173
ali56545 <alikrasava976@gmail.com>
2025-11-03 18:26:00 +0600

    New functions inside

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 from extract.extract_data import extract_weather_data
 from transform.transform_data import transform_weather_data
 from load.load_data import load_weather_data
+from app.config_loader import load_config
 
 def main():
     print("🚀 Starting ETL pipeline...")
 
+    # Load config
+    config = load_config()
+    cities = config["cities"]
+
     # 1. Extract
-    df = extract_weather_data()
-    print("✅ Data extracted!")
+    df = extract_weather_data(cities)
+    print("✅ Data extracted for all cities!")
 
     # 2. Transform
     df = transform_weather_data(df)
