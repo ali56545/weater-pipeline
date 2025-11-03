a3f00b695a13f52a99abaf841049d46f7479cd44
ali56545 <alikrasava976@gmail.com>
2025-11-03 18:25:17 +0600

    New code inside

--- app/extract/extract_data.py
+++ app/extract/extract_data.py
@@ -1,19 +1,27 @@
 import requests
 import pandas as pd
 
-def extract_weather_data():
+def extract_weather_data(cities):
     url = "https://api.open-meteo.com/v1/forecast"
-    params = {
-        "latitude": 42.87,  # Бишкек
-        "longitude": 74.59,
-        "hourly": "temperature_2m",
-        "forecast_days": 1
-    }
-    response = requests.get(url, params=params)
-    data = response.json()
+    all_data = []
 
-    df = pd.DataFrame({
-        "time": data["hourly"]["time"],
-        "temperature": data["hourly"]["temperature_2m"]
-    })
-    return df
+    for city in cities:
+        params = {
+            "latitude": city["lat"],
+            "longitude": city["lon"],
+            "hourly": "temperature_2m",
+            "forecast_days": 1
+        }
+
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        df = pd.DataFrame({
+            "time": data["hourly"]["time"],
+            "temperature": data["hourly"]["temperature_2m"]
+        })
+        df["city"] = city["name"]
+        all_data.append(df)
+
+    full_df = pd.concat(all_data, ignore_index=True)
+    return full_df
